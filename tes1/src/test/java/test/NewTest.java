40911258a24633747bf3b5fa27e1f626b1a37abe
Sreedevi <Sreedevi@DESKTOP-F9MKETK>
2020-06-27 13:57:57 +0530

    test new

--- tes1/src/test/java/test/NewTest.java
+++ tes1/src/test/java/test/NewTest.java
@@ -2,6 +2,7 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,8 +13,10 @@ public class NewTest {
 	
   @Test
   public void openMyBlog() {
-	  System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+	  System.setProperty("webdriver.chrome.driver","chromedrivercvbvc.exe");
 
 	   driver = new ChromeDriver();
 	driver.get("https://www.google.com/");
+	
+	//Assert.assertEquals(text, search_text, "Text not found!");
   }}
